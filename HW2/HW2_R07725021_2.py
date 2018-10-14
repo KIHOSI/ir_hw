d6eb354790dd7d20df72917d59df540328e6ae03
KIHOSI <amie12349@gmail.com>
2018-10-14 18:42:30 +0800

    export all 1095 documents' terms' tf-idf and it runs too longgit add .!

--- HW2/HW2_R07725021_2.py
+++ HW2/HW2_R07725021_2.py
@@ -2,21 +2,19 @@ import math
 
 def findIDF(key,value): #去dictonary.txt找到該term的df和其t_index，將df變idf
     with open('dictionary.txt','r') as f:
+        exit_flag = False #用以跳出for迴圈
         for line in f:
             (index_df,key_df,df) = line.split()
-            #print("%s %s %s\n" % (index_df,key_df,df))
             if(key == key_df):
-                #print("df:%f\n" % float(df))
-                #print("1095/df: %f" % (1095/float(df)))
-                #print("log10(1095/df): %f" % (math.log(1095/float(df))))
-                idf = math.log10(1095/float(df))
-                #print("idf:%f\n" % idf)
+                idf = math.log10(1095/float(df))    
                 tf_idf = float(value)*float(idf)
-                #print("tf-idf:%f\n" % tf_idf)
-                return int(index_df),float(tf_idf)      
+                exit_flag = True
+                return int(index_df),float(tf_idf)
+            if(exit_flag):
+                break          
 
 #讀取TermFrequency每一個doc的term的tf
-for i in range(1,2):
+for i in range(1,1096):
     dict_now = {} # 儲存該doc的term和tf
     # file to dictionary
     with open('TermFrequency/'+str(i)+'.txt','r') as f:
@@ -25,13 +23,11 @@ for i in range(1,2):
             dict_now[key] = value
 
     #去dictonary.txt找到該term的df和其t_index，將df變idf
-    #print("dict_now: %d" % len(dict_now))
-    #print("dict_now(key): %d" % len(dict_now.keys()))
     open('TF_IDF/'+str(i)+'.txt','w').close() # clear doc contents
     with open('TF_IDF/'+str(i)+'.txt','w') as f:
         f.write(str(len(dict_now))+"\n\n") # how many terms in this doc
 
-    for key,tf in dict_now.items():
+    for key,tf in dict_now.items(): #count tf-idf
         t_index,tf_idf = findIDF(key,tf)
 
         with open('TF_IDF/'+str(i)+'.txt','a') as f: #t_index,tf-idf
