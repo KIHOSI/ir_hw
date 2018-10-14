bc7720ff9a1ff0446cf998fa2ef47b480b35f2f6
KIHOSI <amie12349@gmail.com>
2018-10-14 16:36:35 +0800

    export every document's term's tf_index,tf-idf and how much terms this doc contains.

--- HW2/HW2_R07725021_2.py
+++ HW2/HW2_R07725021_2.py
@@ -1,43 +1,39 @@
-from nltk.tokenize import word_tokenize
 import math
 
-def findIDF(key,value):
+def findIDF(key,value): #去dictonary.txt找到該term的df和其t_index，將df變idf
     with open('dictionary.txt','r') as f:
         for line in f:
             (index_df,key_df,df) = line.split()
             #print("%s %s %s\n" % (index_df,key_df,df))
             if(key == key_df):
-                print("df:%f\n" % float(df))
-                print("1095/df: %f" % (1095/float(df)))
-                print("log10(1095/df): %f" % (math.log(1095/float(df))))
+                #print("df:%f\n" % float(df))
+                #print("1095/df: %f" % (1095/float(df)))
+                #print("log10(1095/df): %f" % (math.log(1095/float(df))))
                 idf = math.log10(1095/float(df))
-                print("idf:%f\n" % idf)
+                #print("idf:%f\n" % idf)
                 tf_idf = float(value)*float(idf)
-                print("tf-idf:%f\n" % tf_idf)
+                #print("tf-idf:%f\n" % tf_idf)
                 return int(index_df),float(tf_idf)      
 
 #讀取TermFrequency每一個doc的term的tf
 for i in range(1,2):
     dict_now = {} # 儲存該doc的term和tf
+    # file to dictionary
     with open('TermFrequency/'+str(i)+'.txt','r') as f:
-        for line in f:
+        for line in f: #一行一行儲存
             (key,value) = line.split()
             dict_now[key] = value
-        #print(dict_now)
+
     #去dictonary.txt找到該term的df和其t_index，將df變idf
-    #with open('dictionary.txt','r') as f:
+    #print("dict_now: %d" % len(dict_now))
+    #print("dict_now(key): %d" % len(dict_now.keys()))
+    open('TF_IDF/'+str(i)+'.txt','w').close() # clear doc contents
+    with open('TF_IDF/'+str(i)+'.txt','w') as f:
+        f.write(str(len(dict_now))+"\n\n") # how many terms in this doc
+
     for key,tf in dict_now.items():
         t_index,tf_idf = findIDF(key,tf)
-            #t_index = 0
-            #tf_idf = 0
-            #for line in f:
-                #(index_df,key_df,df) = line.split()
-                #if(key == key_df): # idf = log10(N/df)
-                    #t_index = int(index_df)
-                    #idf = math.log10(1095/float(df))
-                    #tf_idf = float(tf)*idf # tf-idf = tf*idf
-                #break
 
-        with open('TF_IDF/'+str(i)+'.txt','a') as f:
+        with open('TF_IDF/'+str(i)+'.txt','a') as f: #t_index,tf-idf
             f.write("%d %f\n" % (t_index,tf_idf))
   
