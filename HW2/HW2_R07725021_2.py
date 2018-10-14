b5aee5679293f20272d854b30a99ecce34cf9b16
KIHOSI <amie12349@gmail.com>
2018-10-14 19:18:24 +0800

    count tf-idf unit vector. Export to TF_IDF/1.txt

--- HW2/HW2_R07725021_2.py
+++ HW2/HW2_R07725021_2.py
@@ -6,15 +6,27 @@ def findIDF(key,value): #去dictonary.txt找到該term的df和其t_index，將df
         for line in f:
             (index_df,key_df,df) = line.split()
             if(key == key_df):
+                #print("df:%f\n" % float(df))
+                #print("1095/df: %f" % (1095/float(df)))
+                #print("log10(1095/df): %f" % (math.log10(1095/float(df))))
                 idf = math.log10(1095/float(df))    
                 tf_idf = float(value)*float(idf)
+                #print("tf-idf:%f\n" % tf_idf)
                 exit_flag = True
                 return int(index_df),float(tf_idf)
             if(exit_flag):
                 break          
 
+def countDocVectorLength(dict): #計算vector length
+    squareNum = 0
+    for key,value in dict.items():
+        squareNum += int(value) * int(value) #tf^2相加
+    sqrtNum = math.sqrt(squareNum) #最後squart roots
+    #print("vector length:%f\n" % sqrtNum)
+    return sqrtNum     
+
 #讀取TermFrequency每一個doc的term的tf
-for i in range(1,1096):
+for i in range(1,2):
     dict_now = {} # 儲存該doc的term和tf
     # file to dictionary
     with open('TermFrequency/'+str(i)+'.txt','r') as f:
@@ -27,9 +39,14 @@ for i in range(1,1096):
     with open('TF_IDF/'+str(i)+'.txt','w') as f:
         f.write(str(len(dict_now))+"\n\n") # how many terms in this doc
 
+    #print("doc %d" %i)    
+    vectorLength = countDocVectorLength(dict_now)    
     for key,tf in dict_now.items(): #count tf-idf
         t_index,tf_idf = findIDF(key,tf)
+        
+        sqrt_tfidf = float(tf_idf/vectorLength)
+        #print("sqrt_tfidf:%f" % sqrt_tfidf)
 
-        with open('TF_IDF/'+str(i)+'.txt','a') as f: #t_index,tf-idf
-            f.write("%d %f\n" % (t_index,tf_idf))
+        with open('TF_IDF/'+str(i)+'.txt','a') as f: #t_index,tf-idf unit vector
+            f.write("%d %f\n" % (t_index,sqrt_tfidf))
   
