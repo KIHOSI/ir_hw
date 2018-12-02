200f5fc52485df043b9564fc2c8ba924525566b3
KIHOSI <amie12349@gmail.com>
2018-12-02 10:59:30 +0800

    feature selection chi-square修正

--- HW3/HW3_R07725021.py
+++ HW3/HW3_R07725021.py
@@ -29,22 +29,26 @@ def tokenize(docid): #term
 
     return removeStopword #回傳tokenize後該文章的term（字串）
 
-def countChiSquare(list_p,list_c,doc_all): #計算該term在該class的chi square總和（p-c,p-notc,a-c,a-notc)
+def countChiSquare(c_p,c_a,notc_p,notc_a,c_all,notc_all,p_all,a_all,doc_all): #計算該term在該class的chi square總和（p-c,p-notc,a-c,a-notc)
+    chi_square_list = []
     chi_square_sum = 0 #總和
-    for i in range(0,2): #p:0,1
-        for j in range(0,2): #c:0,1
-            # print("list_p:"+str(i)+" "+str(list_p[i]))
-            # print("list_c:"+str(j)+" "+str(list_c[j]))
-            # print("doc_all:"+str(doc_all))
-            expected_count = doc_all*(list_p[i]/doc_all)*(list_c[j]/doc_all)
-            # print("expected_count:"+ str(expected_count))
-            chi_square = ((doc_all - expected_count)**2)/expected_count
-            # print("chi_square:"+ str(chi_square))
+    for i in range(0,13): #計算每一個class的chi-square
+        list_p = [p_all[i],a_all[i],p_all[i],a_all[i]]
+        list_c = [c_all,c_all,notc_all,notc_all]
+        list_observed = [c_p[i],c_a[i],notc_p[i],notc_a[i]]
+        for j in range(0,4): #四個chi-square加總
+            # for k in range(0,2):
+            observed_frequency = list_observed[j]
+            expected_count = doc_all*(list_p[j]/doc_all)*(list_c[j]/doc_all)
+            chi_square = ((observed_frequency- expected_count)**2)/expected_count
             chi_square_sum += chi_square
-    # print("chi_square_sum:"+ str(chi_square_sum))
-    return chi_square_sum
+        chi_square_list.append(chi_square_sum)        
+    
+    # print(chi_square_list)
 
-def chiSquare(term,dict_class,dict_train_doc): #計算每個term在13個class各別的chi-square，回傳13個裡面最大的值！
+    return max(chi_square_list) #回傳最高的chi-square值
+
+def contigencyTable(term,dict_class,dict_train_doc): #計算每個term在13個class各別的chi-square，回傳13個裡面最大的值！
     #計算個chi squares需要的值（c-p,c-a,notc-p,notc-a,c-all,p-all,doc_all)
     doc_all = 195
     c_all = 15
@@ -59,14 +63,14 @@ def chiSquare(term,dict_class,dict_train_doc): #計算每個term在13個class各
                 count_df += 1
         c_p.append(count_df)  #這個term在這個class的df值
 
-    p_all = 0 #[all present]
+    # p_all = 0 #[all present]
     c_a = [] #[class-absent]
     notc_p = [] #[not class-present]
     notc_a = [] #[not class-absent]
-    a_all = 0 #[absent-all]
+    # a_all = 0 #[absent-all]
 
     for i in range(0,13): #第幾個class
-        p_all += c_p[i] #得p_all
+        # p_all += c_p[i] #得p_all
         c_a.append(15-c_p[i]) #c_all
 
         count_notc_p = 0 #計算各個class中not c-present的值
@@ -76,13 +80,20 @@ def chiSquare(term,dict_class,dict_train_doc): #計算每個term在13個class各
             count_notc_p += c_p[j]
         notc_p.append(count_notc_p)
         notc_a.append(notc_all - notc_p[i])
-    a_all += doc_all - p_all
+    # a_all += doc_all - p_all
+
+    #計算各別class的p_all,a_all
+    p_all = []
+    a_all = []
+    for i in range(0,13):
+        p_all.append(c_p[i]+notc_p[i])
+        a_all.append(c_a[i]+notc_a[i])
 
     #這個算法為計算該term在全部class的4個chi square值，加總！（就算個別算，值也一樣）
     #因為四個值會加總，最終p_all,c_all,a_all,notc_all都一樣
-    list_p = [p_all,a_all]
-    list_c = [c_all,notc_all]
-    chi_square_sum = countChiSquare(list_p,list_c,doc_all)   
+    # list_p = [p_all,a_all]
+    # list_c = [c_all,notc_all]
+    chi_square_sum = countChiSquare(c_p,c_a,notc_p,notc_a,c_all,notc_all,p_all,a_all,doc_all)   
     # print(chi_square_sum)
 
     return chi_square_sum 
@@ -141,8 +152,6 @@ def ApplyMultinomialNB(dict_class,test_data,condprob,prior_c): # multinomial mod
 dict_doc = {} #儲存tokenize後的1095文章的term
 for i in range(1,1096): #將1095個doc做tokenize
     dict_doc[str(i)] = tokenize(str(i)) #key為docid,value為該doc的所有term (i和docid轉成string)
-    #print("dict_doc key:"+str(i)+"\n")
-    #print(dict_doc)
 
 #讀取training.txt，得到各class及對應的training docid
 dict_class = {} # key為classid,value為docid_list
@@ -161,7 +170,6 @@ for key,value in dict_class.items(): # key為classid,value為docid_list
         dict_train_doc[docid] = tokens
 
 #去除train doc，剩下1095-195=900個test doc
-# print(len(dict_doc))
 dict_test_doc = dict_doc.copy() #複製1095個doc
 # print(len(dict_test_doc))
 for classid,docid_list in dict_class.items():
@@ -180,7 +188,7 @@ dict_chisquare = {} #key為term,value為chisquare值
 for terms_list in dict_train_doc.values(): #key為docid,value為terms，取得每一個term
     for term in terms_list:
         # print(term)
-        chisquare = chiSquare(term,dict_class,dict_train_doc)
+        chisquare = contigencyTable(term,dict_class,dict_train_doc)
         if(dict_chisquare.get(term)): #有重複的term,跳過
             continue
         dict_chisquare[term] = chisquare
@@ -195,7 +203,7 @@ for key,value in sorted(dict_chisquare.items(), key = lambda x:x[1],reverse=True
     feature_selection_list.append(key)
     count += 1    
 
-# print(len(feature_selection_list))
+print(len(feature_selection_list))
 # print(feature_selection_list)
 
 #過濾train data和test data，只剩這500個term
@@ -209,6 +217,7 @@ for docid,terms in dict_test_doc.items(): #將test data過濾
     filter_terms = [term for term in terms if term in feature_selection_list]  #只留下在feature selection後這500個term的字
     dict_test_doc_filter[docid] = filter_terms
 
+print(dict_train_doc_filter)
 # print(dict_test_doc_filter)
 
 #分類
